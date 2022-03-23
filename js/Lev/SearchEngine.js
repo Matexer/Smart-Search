041e7dbcb7a529ee9e7893a96565966cc5e5a552
Mateusz <mwojciechowski296@gmail.com>
2022-03-23 09:34:15 +0100

    l

--- js/Lev/SearchEngine.js
+++ js/Lev/SearchEngine.js
@@ -1,6 +1,14 @@
 export class SearchEngine {
     static encodingVal = "";
-    static SizeTVal = "8";
+    static deletionCost = 1;
+    static insertionCost = 1;
+
+
+    static lookFor(pattern, text, maxDistance) {
+        var SizeTVal = this.getSizeTVal(pattern);
+        let Engine = this.getEngine(SizeTVal);
+        return Engine.lookFor(pattern, text, maxDistance);
+    }
 
     static setEncoding(encoding) {
         switch (encoding) {
@@ -18,15 +26,15 @@ export class SearchEngine {
         }
     }
 
-    static setSizeTVal(pattern) {
+    static getSizeTVal(pattern) {
         if (pattern.length < 256) {
-            this.SizeTVal = "8";
+            return 8;
         }
         else if (pattern.length < 65536) {
-            this.SizeTVal = "16";
+            return 16;
         }
         else if (pattern.length < 4294967296) {
-            this.SizeTVal = "32";
+            return 32;
         }
         else {
             console.error("Too long pattern. Max length is 4 294 967 295")
@@ -34,45 +42,44 @@ export class SearchEngine {
         }
     }
 
-    static getEngine() {
-        if (this.SizeTVal == "8") {
+    static getEngine(SizeTVal) {
+        switch(SizeTVal) {
+
+        case(8):
             if (this.encodingVal == "") {
                 return Module.SearchEngineStr8;
             }
-            if (this.encodingVal == "16") {
+            else if (this.encodingVal == "16") {
                 return Module.SearchEngine16Str8;
             }
             else {
                 return Module.SearchEngine32Str8;
             }
-        }
-        else if (this.SizeTVal == "16") {
+        break
+
+        case(16): 
             if (this.encodingVal == "") {
                 return Module.SearchEngineStr16;
             }
-            if (this.encodingVal == "16") {
+            else if (this.encodingVal == "16") {
                 return Module.SearchEngine16Str16;
             }
             else {
                 return Module.SearchEngine32Str16;
             }
-        }
-        else {
+        break
+
+        default:
             if (this.encodingVal == "") {
                 return Module.SearchEngineStr32;
             }
-            if (this.encodingVal == "16") {
+            else if (this.encodingVal == "16") {
                 return Module.SearchEngine16Str32;
             }
             else {
                 return Module.SearchEngine32Str32;
             }
-        }
+        break
     }
-
-    static lookFor(pattern, text, maxDistance) {
-        this.setSizeTVal(pattern);
-        let Engine = this.getEngine();
-        return Engine.lookFor(pattern, text, maxDistance);
     }
 };
---
+++ js/content.js
@@ -0,0 +1,8 @@
+function getDOMContent() {
+
+};
+
+
+function getRedableContent(DOMContent) {
+
+};
