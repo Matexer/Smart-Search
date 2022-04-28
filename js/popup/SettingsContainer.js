a5396c98018bb4211cee5e4d2a854709be9c357f
Mateusz <mwojciechowski296@gmail.com>
2022-04-28 17:25:46 +0200

    refactoring

--- js/popup/SettingsContainer.js
+++ js/popup/SettingsContainer.js
@@ -33,33 +33,33 @@ export class SettingsContainer extends Container {
     }
 
     getSettings() {
-        return {
-            language: $('#language').val(),
-            capitalLetters: $('#capital-letters-chbox').prop('checked'),
-            maxNumOfOutputs: $('#max-num-of-outputs').val(),
-            defaultMaxDistance: $('#def-max-distance').val(),
-            deletionCost: $('#deletion-cost').val(),
-            insertionCost: $('#insertion-cost').val(),
-            swapCost: $('#swap-cost').val(),
-            purifyRange: $('#purify-range').val(),
-            fixRange: $('#fix-range').val(),
-            utfEncoding: $('#utf-encoding').val(),
-            multithreading: $('#multithreading-chbox').prop('checked')
-        };
+        let settings = {};
+        let id = null;
+
+        for(let setting in this._settingsId) {
+            id = this._settingsId[setting];
+            settings[setting] = $(id).val();
+        }
+
+        for(let chbox in this._chboxesId) {
+            id = this._chboxesId[chbox];
+            settings[chbox] = $(id).prop('checked');
+        }
+
+        return settings;
     }
 
     insertSettings(settings) {
-        $('#language').val(settings.language);
-        $('#capital-letters-chbox').prop('checked', settings.capitalLetters);
-        $('#max-num-of-outputs').val(settings.maxNumOfOutputs);
-        $('#def-max-distance').val(settings.defaultMaxDistance);
-        $('#deletion-cost').val(settings.deletionCost);
-        $('#insertion-cost').val(settings.insertionCost);
-        $('#swap-cost').val(settings.swapCost);
-        $('#purify-range').val(settings.purifyRange);
-        $('#fix-range').val(settings.fixRange);
-        $('#utf-encoding').val(settings.utfEncoding);
-        $('#multithreading-chbox').prop('checked', settings.multithreading);
+        let id = null;
+        for(let setting in this._settingsId) {
+            id = this._settingsId[setting];
+            $(id).val(settings[setting]);
+        }
+
+        for(let chbox in this._chboxesId) {
+            id = this._chboxesId[chbox];
+            $(id).prop('checked', settings[chbox]);
+        }
     }
 
     unmarkAll() {
